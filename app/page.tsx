e7b5668cf14b8159b081c02cf22909f4823a4c96
Sumit <sumitgarg380@gmail.com>
2025-03-25 20:56:54 +0530

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,37 @@
+'use client'
 import Image from "next/image";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Home() {
+  const [width,setWidth] = useState(0);
+  const [height,setHeight] = useState(0);
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight
+    );
+  }, [window.innerWidth,window.innerHeight]);
+  
   return (
     <div>
       <div className="w-full h-screen flex justify-center items-center sm:hidden bg-black">
         <Image
           src="/movile.jpg"
           alt="Picture of the author"
-          width={500}
-          height={500}
+          width={width}
+          height={width}
         />
       </div>
       <div
-        className="w-full h-screen justify-center items-center bg-cover bg-center hidden sm:block"
-        style={{
-          backgroundImage: 'url("/pic.jpg")',
-        }}
+        className="w-full min-h-screen justify-center items-center bg-cover bg-center hidden sm:block overflow-auto"
+        
       >
+        <Image
+          src="/pic.jpg"
+          alt="Picture of the author"
+          width={width}
+          height = {height}
+        />
         {/* Other content can go here */}
       </div>
     </div>
